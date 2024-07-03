fe000121099ef1bae66572a25d66c9c23f00cbfd
BarkovFA <barkov8@mail.ru>
2024-07-03 22:40:14 +0300

    new task

---
+++ src/task3/Main.java
@@ -0,0 +1,21 @@
+package task3;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
+
+class ComplexNumber {
+    int numInt;
+    int imaginary;
+
+
+
+
+    public ComplexNumber(int numInt, int imaginary) {
+        this.imaginary = imaginary;
+        this.numInt = numInt;
+    }
+
+}
