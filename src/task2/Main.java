d6654299f2c84c05c6aadccaf4cf82b1ea682d32
BarkovFA <barkov8@mail.ru>
2024-07-03 21:27:47 +0300

    new task

---
+++ src/task2/Main.java
@@ -0,0 +1,33 @@
+package task2;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
+
+class TimeSpan {
+    private int hour;
+    private int minute;
+    private int second;
+
+    public TimeSpan (int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+
+    void add(TimeSpan time) {
+
+    }
+
+    void subtract(TimeSpan time) {
+
+    }
+
+    public String toString() {
+
+        return "()";
+    }
+}
