e25ea3a2d975cace8e4877c3f6597b2f654ae746
BarkovFA <barkov8@mail.ru>
2024-07-10 21:13:04 +0300

    correct

--- src/task11/Main.java
+++ src/task11/Main.java
@@ -2,14 +2,16 @@ package task11;
 
 import java.io.*;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         byte [] lst = new byte[] {48, 49, 50, 51};
         ByteArrayInputStream inputStream = new ByteArrayInputStream(lst);
-        String str = readAsString(inputStream, Charset.forName("ASCII"));
+        String str = readAsString(inputStream, StandardCharsets.US_ASCII);
         System.out.println(str);
     }
+
 
     public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
         Reader reader = new InputStreamReader(inputStream, charset);
