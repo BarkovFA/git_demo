e5390772b45c69e4dfd4dae6663a1eded0fb3bb6
BarkovFA <barkov8@mail.ru>
2024-07-12 00:07:23 +0300

    new task

---
+++ src/task15/Main.java
@@ -0,0 +1,19 @@
+package task15;
+
+
+class Pair <X, Y> {
+
+
+    public X getFirst() {
+        return ;
+    }
+
+    public Y getSecond() {
+        return ;
+    }
+
+    public boolean equals(){
+
+    }
+
+}
