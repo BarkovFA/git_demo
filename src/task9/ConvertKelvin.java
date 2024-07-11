9822af83cb06919e21333975a533ea35f27da637
BarkovFA <barkov8@mail.ru>
2024-07-11 16:46:51 +0300

    correct

--- src/task9/ConvertKelvin.java
+++ src/task9/ConvertKelvin.java
@@ -2,9 +2,15 @@ package task9;
 
 public class ConvertKelvin extends BaseConverter{
 
+    @Override
     public double getConvert(double degrees)  {
         double kelvin = degrees + 273.1;
-        setLastDegrees(degrees, kelvin, "C");
+        setLastDegrees(degrees, kelvin);
         return kelvin;
+    }
+
+    @Override
+    protected String getSymbol() {
+        return "K";
     }
 }
