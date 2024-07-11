7f07acc648efc58f8f6afb9e68734cb2044be43b
BarkovFA <barkov8@mail.ru>
2024-07-11 15:34:41 +0300

    correct

--- src/task5/Main.java
+++ src/task5/Main.java
@@ -5,16 +5,21 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Item item1 = new Item("Table", 500.5);
-        Item item2 = new Item("Mouse", 500.5);
-        Bill bill1 = new Bill();
-        Bill bill2 = new Bill();
-        System.out.println("Price without sale: " + bill1.getCost());
-        DiscountBill discountBill = new DiscountBill(5);
-        discountBill.addItem(item1, 12);
-        discountBill.addItem(item2, 2);
-        System.out.println("Sale, % : " + discountBill.getDiscount());
-        System.out.println("Total price without sale: " + discountBill.getCost());
+        Item item1 = new Item("Table", 10.0);
+        Item item2 = new Item("Laptop", 100.5);
+
+        Bill bill = new Bill();
+        bill.addItem(item1, 5);
+        bill.addItem(item2, 10);
+
+        System.out.println("Total without discount: " + bill.getTotal());
+
+        DiscountBill discountBill = new DiscountBill(10);
+        discountBill.addItem(item1, 5);
+        discountBill.addItem(item2, 10);
+
+        System.out.println("Total with discount: " + discountBill.getTotal());
+        System.out.println("Discount amount: " + discountBill.getDiscount());
     }
 }
 
@@ -39,23 +44,29 @@ class Item {
 
 class Bill {
     private Map <Item, Integer> items;
-    double sum = 0.0;
+    private double sum;
 
     public Bill() {
         this.items = new HashMap<>();
+        sum = 0.0;
     }
     //add things in list
     public void addItem(Item item, int cost) {
         items.put(item, items.getOrDefault(item, 0) + cost);
+        sum += item.getPrice() * cost;
+    }
+
+    public double getTotal() {
+        return sum;
     }
     //all price
-    public double getCost() {
-        double totalPrice = 0.0;
-        for(Map.Entry<Item, Integer> entry: items.entrySet()) {
-            totalPrice += entry.getKey().getPrice() * entry.getValue();
-        }
-        return totalPrice;
-    }
+//    public double getCost() {
+//        double totalPrice = 0.0;
+//        for(Map.Entry<Item, Integer> entry: items.entrySet()) {
+//            totalPrice += entry.getKey().getPrice() * entry.getValue();
+//        }
+//        return totalPrice;
+//    }
 
     public Map<Item, Integer> getItems() {
         return items;
@@ -74,18 +85,13 @@ class DiscountBill extends Bill{
         return discount;
     }
 
-    @Override
-    public double getCost() {
-        double totalNotDisc = super.getCost();
-        double totalDic = totalNotDisc * (discount / 100);
-        double lastPrice = totalNotDisc - totalDic;
-        return lastPrice;
-    }
 
-    public double getDisc() {
-        double totalNotDisc = super.getCost();
-        double totalDic = totalNotDisc * (discount / 100);
-        return totalDic;
+    public double getCost() {
+        return getTotal() * (discount / 100);
+    }
+    @Override
+    public double getTotal() {
+        return super.getTotal() - getCost();
 
     }
 }
