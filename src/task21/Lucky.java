a754566b47125044455ab87d19570e7fcd92f1d9
Барков Федор Андреевич <f.barkov@bftcom.com>
2024-09-10 21:05:47 +0300

    correct

--- src/task21/Lucky.java
+++ src/task21/Lucky.java
@@ -3,16 +3,21 @@ package task21;
 public class Lucky {
     static int x = 0;
     static int count = 0;
+    static Object block = new Object();
 
     static class LuckyThread extends Thread {
         @Override
         public void run() {
-            while (x < 999999) {
-                x++;
-                if ((x % 10) + (x / 10) % 10 + (x / 100) % 10 == (x / 1000)
-                        % 10 + (x / 10000) % 10 + (x / 100000) % 10) {
-                    System.out.println(x);
-                    count++;
+            synchronized (block) {
+                while (x < 999999) {
+                    x++;
+                    if ((x % 10) + (x / 10) % 10 + (x / 100) % 10 == (x / 1000)
+                            % 10 + (x / 10000) % 10 + (x / 100000) % 10) {
+                        System.out.println(x);
+                        count++;
+
+
+                    }
                 }
             }
         }
@@ -23,11 +28,13 @@ public class Lucky {
         Thread t2 = new LuckyThread();
         Thread t3 = new LuckyThread();
         t1.start();
-        t1.join();
         t2.start();
-        t2.join();
         t3.start();
+        t1.join();
+        t2.join();
         t3.join();
         System.out.println("Total: " + count);
     }
 }
+
+
