ffe8e41a530d437adf4de685b2b8b228ee37ef4d
Барков Федор Андреевич <f.barkov@bftcom.com>
2024-09-11 11:12:17 +0300

    correct

--- src/task22/PoolExample.java
+++ src/task22/PoolExample.java
@@ -13,7 +13,7 @@ public class PoolExample {
         //   максимальное количество созданных задач - 3
         ThreadPoolExecutor executor = new ThreadPoolExecutor(
                 // не изменяйте эти параметры
-                3, 3, 1, TimeUnit.SECONDS, new LinkedBlockingQueue<>(30));
+                3, 3, 1, TimeUnit.SECONDS, new LinkedBlockingQueue<>(3));
 
         // сколько задач выполнилось
         AtomicInteger count = new AtomicInteger(0);
@@ -22,26 +22,29 @@ public class PoolExample {
         AtomicInteger inProgress = new AtomicInteger(0);
 
         // отправляем задачи на выполнение
-        for (int i = 0; i < 30; i++) {
-            final int number = i;
-            Thread.sleep(10);
-
-            System.out.println("creating #" + number);
-            executor.execute(() -> {
-                int working = inProgress.incrementAndGet();
-                System.out.println("start #" + number + ", in progress: " + working);
-                try {
-                    // тут какая-то полезная работа
-                    Thread.sleep(Math.round(1000 + Math.random() * 2000));
-                } catch (InterruptedException e) {
-                    // ignore
-                }
-                working = inProgress.decrementAndGet();
-                System.out.println("end #" + number + ", in progress: " + working + ", done tasks: " + count.incrementAndGet());
-            });
+        int i = 0;
+        while (i < 30) {
+            if (inProgress.get() < 3) {
+                final int number = i;
+                System.out.println("creating #" + number);
+                executor.submit(() -> {
+                    int working = inProgress.incrementAndGet();
+                    System.out.println("start #" + number + ", in progress: " + working);
+                    try {
+                        // тут какая-то полезная работа
+                        Thread.sleep(Math.round(1000 + Math.random() * 2000));
+                    } catch (InterruptedException e) {
+                        // ignore
+                    }
+                    working = inProgress.decrementAndGet();
+                    System.out.println("end #" + number + ", in progress: " + working + ", done tasks: " + count.incrementAndGet());
+                });
+                i++;
+            } else {
+                Thread.sleep(10);
+            }
         }
         executor.shutdown();
         executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
-        System.out.println("Вывести текст");
     }
 }
